cef0655f5e8fdd2b42c919944f26d9a9436c31ea
Martin Viljoen <mailmartinviljoen@gmail.com>
2025-03-21 07:43:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,51 +44,55 @@ for serialitem in inverterserials:
         varContest = postapi.ConnectionTest("TEST","A","current",f"connection_test",f"connection_test_current","100")
         if varContest=="Connection Success":
             print(varContest)
+            
+            print("--------------------------------------")
+            print("--" + ConsoleColor.WARNING + "Getting Inverter Information")
+            print("--------------------------------------")
+            getapi.GetInverterInfo(BearerToken,str(serialitem))
+            print("--------------------------------------")
+            print("--" + ConsoleColor.WARNING + "Getting PV Information")
+            print("--------------------------------------")
+            getapi.GetPvData(BearerToken,str(serialitem))
+            print("--------------------------------------")
+            print("--" +  ConsoleColor.WARNING + "Getting GRID Information")
+            print("--------------------------------------")
+            getapi.GetGridData(BearerToken,str(serialitem))
+            print("--------------------------------------")
+            print("--" +  ConsoleColor.WARNING + "Getting BATTERY Information")
+            print("--------------------------------------")
+            getapi.GetBatteryData(BearerToken,str(serialitem))
+            print("--------------------------------------")
+            print("--" +  ConsoleColor.WARNING + "Getting LOAD Information")
+            print("--------------------------------------")
+            getapi.GetLoadData(BearerToken,str(serialitem))
+            print("--------------------------------------")
+            print("--" +  ConsoleColor.WARNING + "Getting OUTPUT Information")
+            print("--------------------------------------")
+            getapi.GetOutputData(BearerToken,str(serialitem))
+            print("--------------------------------------")
+            print("--" +  ConsoleColor.WARNING + "Getting DC & AC Temperature  Information")
+            print("--------------------------------------")
+            getapi.GetDCACTemp(BearerToken,str(serialitem))
+            
+            #getapi.GetInverterSerials(BearerToken,str(serialitem))
+            #settingsmanager.DownloadSunSynkSettings(BearerToken,str(serialitem))  
+            #settingsmanager.GetNewSettingsFromHAEntity(BearerToken,str(serialitem)) 
+
+            #source_file = "svr_settings.json"
+            #target_file = "battery_settings.json"
+            #output_file = "merged_battery_settings.json"        
+            #settingsmanager.merge_json_settings(source_file, target_file, output_file)
+            #DEBUG Read contents of newlybuilt battery_settings file
+            #with open("merged_battery_settings.json", "r") as file:
+            #    content = file.read()
+            #print(content)            
         else:
             print(varContest)
             print(ConsoleColor.MAGENTA + "Ensure you’ve entered the correct IP address and port without http:// or https:// prefixes. If Home Assistant has SSL enabled, be sure to check Enable_HTTPS. If this Docker container is running on a different machine, verify that the Home Assistant server is accessible." + ConsoleColor.ENDC)
         
-        print("--------------------------------------")
-        print("--" + ConsoleColor.WARNING + "Getting Inverter Information")
-        print("--------------------------------------")
-        getapi.GetInverterInfo(BearerToken,str(serialitem))
-        print("--------------------------------------")
-        print("--" + ConsoleColor.WARNING + "Getting PV Information")
-        print("--------------------------------------")
-        getapi.GetPvData(BearerToken,str(serialitem))
-        print("--------------------------------------")
-        print("--" +  ConsoleColor.WARNING + "Getting GRID Information")
-        print("--------------------------------------")
-        getapi.GetGridData(BearerToken,str(serialitem))
-        print("--------------------------------------")
-        print("--" +  ConsoleColor.WARNING + "Getting BATTERY Information")
-        print("--------------------------------------")
-        getapi.GetBatteryData(BearerToken,str(serialitem))
-        print("--------------------------------------")
-        print("--" +  ConsoleColor.WARNING + "Getting LOAD Information")
-        print("--------------------------------------")
-        getapi.GetLoadData(BearerToken,str(serialitem))
-        print("--------------------------------------")
-        print("--" +  ConsoleColor.WARNING + "Getting OUTPUT Information")
-        print("--------------------------------------")
-        getapi.GetOutputData(BearerToken,str(serialitem))
-        print("--------------------------------------")
-        print("--" +  ConsoleColor.WARNING + "Getting DC & AC Temperature  Information")
-        print("--------------------------------------")
-        getapi.GetDCACTemp(BearerToken,str(serialitem))
-        
-        #getapi.GetInverterSerials(BearerToken,str(serialitem))
-        #settingsmanager.DownloadSunSynkSettings(BearerToken,str(serialitem))  
-        #settingsmanager.GetNewSettingsFromHAEntity(BearerToken,str(serialitem)) 
 
-        #source_file = "svr_settings.json"
-        #target_file = "battery_settings.json"
-        #output_file = "merged_battery_settings.json"        
-        #settingsmanager.merge_json_settings(source_file, target_file, output_file)
-        #DEBUG Read contents of newlybuilt battery_settings file
-        #with open("merged_battery_settings.json", "r") as file:
-        #    content = file.read()
-        #print(content)          
+        
+          
         
         
         
@@ -97,9 +101,3 @@ for serialitem in inverterserials:
         
     else:
         print(f"Something went wrong for inverter serial: {serialitem}")
-
-
-
-
-
-
