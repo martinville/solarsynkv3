4db7be40ad9396d635247f9270b56596771ee0cf
Jan Jouke Tjalsma <jan-jouke.tjalsma@trivago.com>
2025-05-08 23:55:34 +0200

    Try relative path.

--- src/util/home_assistant_options.py
+++ src/util/home_assistant_options.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from .console import ConsoleColor
-from src.config.home_assistant import OPTIONS_JSON_PATH_HOME_ASSISTANT
+from ..config.home_assistant import OPTIONS_JSON_PATH_HOME_ASSISTANT
 
 
 def get():
