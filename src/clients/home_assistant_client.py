eed2d1ecbbb5a6a7fe0a164295df2f44bd97d1dd
Paul Davies <paulmdavies@gmail.com>
2025-09-20 17:58:50 +0100

    Add setting to use Supervisor API

--- src/clients/home_assistant_client.py
+++ src/clients/home_assistant_client.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import requests
 from requests import Response
@@ -9,19 +10,24 @@ class HomeAssistantClient:
         with open('/data/options.json') as options_file:
             json_settings = json.load(options_file)
 
-            HAToken = json_settings['HA_LongLiveToken']
-
-            if json_settings['Enable_HTTPS']:
-                httpurl_proto = "https"
+            if json_settings.get('use_internal_api', False):
+                print("Using internal API")
+                self.base_url = f"http://supervisor/core"
+                token = os.environ['SUPERVISOR_TOKEN']
             else:
-                httpurl_proto = "http"
+                print('Using API as specified in options.json')
+                token = json_settings['HA_LongLiveToken']
 
-                # print(json_settings['Enable_HTTPS'])
-            # API URL
-            self.base_url = f"{httpurl_proto}://" + str(json_settings['Home_Assistant_IP']) + ":" + str(
-                json_settings['Home_Assistant_PORT'])
+                if json_settings['Enable_HTTPS']:
+                    httpurl_proto = "https"
+                else:
+                    httpurl_proto = "http"
 
-            self.headers = {"Content-Type": "application/json","Authorization": f"Bearer {HAToken}"}
+                # API URL
+                self.base_url = f"{httpurl_proto}://" + str(json_settings['Home_Assistant_IP']) + ":" + str(
+                    json_settings['Home_Assistant_PORT'])
+
+            self.headers = {"Content-Type": "application/json","Authorization": f"Bearer {token}"}
 
     def post(self, path: str, payload: dict) -> Response:
         return requests.post(self.base_url + path, json=payload, headers=self.headers, timeout=10, verify=False)
