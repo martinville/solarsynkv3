b1dc2b190e070dc9460d042d751452832d2df961
Martin Viljoen <mailmartinviljoen@gmail.com>
2026-06-20 06:22:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,9 @@ if BearerToken:
                 (getapi.GetLoadData, "Load Data"),
                 (getapi.GetOutputData, "Output Data"),
                 (getapi.GetDCACTemp, "DC & AC Temperature Data"),
-                (getapi.GetInverterSettingsData, "Inverter Settings")
+                (getapi.GetInverterSettingsData, "Inverter Settings"),
+                (getapi.GetFlowData, "Flow Data")
+                
             ]
 
             # Start threaded API calls
